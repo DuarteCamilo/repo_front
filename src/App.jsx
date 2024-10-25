196c70a6214fd2efd2aa2e25471d0e8fe574dabe
blandoncj <jacoboblandon94@gmail.com>
2024-10-25 07:35:27 -0500

    feat: add admin home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// layouts 
+import AuthLayout from './layouts/auth/AuthLayout'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Error404 from './pages/Error404'
+import AdminHome from './pages/admin/AdminHome'
+
 function App() {
   return (
-    <>
-      <h1 className="bg bg-red-300">Hello, World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<Login />}></Route>
+          <Route path='signup' element={<Register />} />
+        </Route>
+        <Route path='/admin/home' element={<AdminHome />} />
+        <Route path='*' element={<Error404 />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import { RiLockLine, RiUserLine } from 'react-icons/ri';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Input from '../../components/Input/Input';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import { loginUser } from '../../services/authService';
+import { areFieldsEmpty, isValidEmail } from '../../helpers/validationHelper';
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+    const navigate = useNavigate()
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (areFieldsEmpty([email, password])) {
+            toast.error('Todos los campos son obligatorios', { theme: 'light' })
+            return
+        }
+
+        if (!isValidEmail(email)) {
+            toast.error('Correo electrónico inválido', {
+                theme: 'light',
+            });
+            return;
+        }
+
+        try {
+            const data = await loginUser(email, password)
+
+            if (data.token) {
+                console.log('Usuario autenticado', data)
+
+                if (data.user.role === 'admin') {
+                    navigate('/admin/home')
+                } else {
+                    console.log('El usuario es paciente')
+                }
+            }
+        } catch (error) {
+            toast.error(error.message || 'Error de conexión', { theme: 'light' })
+        }
+    };
+
+    return (
+        <div className='bg-white rounded-lg shadow-lg flex flex-col lg:flex-row w-full max-w-4xl overflow-hidden'>
+
+            <div className='flex-1 p-8'>
+                <h1 className='text-4xl font-bold text-gray-800'>Bienvenido a</h1>
+                <h2 className='text-5xl font-bold text-blue-500'>Adiós Caries</h2>
+                <p className='mt-4 text-gray-600'>
+                   Inicia sesión para acceder a tu cuenta: agenda tus citas y gestiona tu salud dental con facilidad. 
+                </p>
+            </div>
+
+            <div className='flex-1 bg-gray-100 p-8'>
+                <form onSubmit={handleSubmit} className="space-y-6">
+
+                    <Input
+                        icon={<RiUserLine />}
+                        type="text"
+                        name="email"
+                        placeholder="Ingresa tu correo electrónico"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <Input
+                        icon={<RiLockLine />}
+                        type="password"
+                        name="password"
+                        placeholder="Ingresa tu contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        showPassword={showPassword}
+                        handleShowPassword={handleShowPassword}
+                    />
+
+                    <SubmitButton text='Iniciar sesión' />
+
+                </form>
+                <p className="text-center text-gray-600 mt-5">
+                    ¿No te encuentras registrado?
+                    <Link to='signup' className='ml-1 text-blue-500 font-semibold hover:underline'>Regístrate</Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
