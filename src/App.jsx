8bc166bf4a6d6d6355c0110e0b2ad332a7990763
blandoncj <jacoboblandon94@gmail.com>
2024-10-28 17:26:42 -0500

    feat: Update environment variables and dependencies
    - Update .env file to include NODE_ENV=development
    - Update package.json and package-lock.json to include dotenv@16.4.5
    - Remove unused react.svg file
    - Add ProtectedRoute component and Sidebar component
    - Update API URL in config.js and handle errors in authService.js
    - Add admin home, patients, and dentists pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import AuthLayout from './layouts/auth/AuthLayout'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import Error404 from './pages/Error404'
-import AdminHome from './pages/admin/AdminHome'
+import Patients from './pages/admin/Patients'
+import Dentists from './pages/admin/Dentists'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 
 function App() {
   return (
@@ -15,7 +17,18 @@ function App() {
           <Route index element={<Login />}></Route>
           <Route path='signup' element={<Register />} />
         </Route>
-        <Route path='/admin/home' element={<AdminHome />} />
+        <Route
+          path='/admin/dentistas'
+          element={
+            <ProtectedRoute element={<Dentists />} />
+          }
+        />
+        <Route
+          path='/admin/pacientes'
+          element={
+            <ProtectedRoute element={<Patients />} />
+          }
+        />
         <Route path='*' element={<Error404 />}></Route>
       </Routes>
     </BrowserRouter>
